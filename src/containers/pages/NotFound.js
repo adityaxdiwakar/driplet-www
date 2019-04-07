28e9f4f654f76d3a8bf3bad94fe4a764dc288c8f
Bottersnike <bottersnike237@gmail.com>
2019-04-07 15:39:56 +0100

    404 page

--- src/containers/pages/NotFound.js
+++ src/containers/pages/NotFound.js
@@ -5,6 +5,11 @@ import AppPage from "../../components/AppPage";
 
 export default () =>
     <AppPage name="NotFound">
-        <h1>404 not found</h1>
-        <Link to="/">Home</Link>
+        <div className={"error-wrap"}>
+            <div className={"error-title"}>404: Not Found</div>
+            <div className={"error-sub1"}>Looks like this page doesn't exist</div>
+            <div className={"error-sub2"}>Take a wrong turn somewhere?</div>
+
+            <Link to="/"><div className={"error-link"}>Get me out of here</div></Link>
+        </div>
     </AppPage>;
